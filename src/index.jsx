8e26dc206ab067c52c8954a643b6c831377c35c5
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-16 10:16:48 +0100

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,86 @@
+import { createRoot } from "react-dom/client";
+
+const root=createRoot(document.getElementById("root"))
+
+/** Mini Challenge:
+ * 
+ * Move the `header` element from the Page component into 
+ * its own component called "Header"
+ * 
+ * Then render an instance of the Header component inside
+ * the Page component where the `header` used to be.
+ */
+
+function Header(){
+    return(
+            <heder>
+                <img src="src/react-1.svg" alt="react logo" width={40}/>
+            </heder>
+    )
+}
+
+
+
+function Page(){
+    return(
+        <>
+            <h2>Reasons why i should learn react: </h2>
+            <ul>
+                <li>1. I need it to my Project</li>
+                <li>2. it's in high demende in job market</li>
+                <li>3. makes you special and weponaize your Skills shield</li>
+            </ul>
+            <footer>© 2025 SkillStack. All rights reserved.</footer>
+        </>
+    )
+}
+
+root.render(
+    <>
+        <Header/>
+        <Page/>
+    </>
+    
+)
+
+// function Call(){
+//     return(
+//         <main>
+//         <img src="src/react-1.svg" width={40} />
+//             <h1>This is react mfs</h1>
+//             <ul>
+//                 <li>Create an h1 element (vanilla JS, not React)</li>
+//                 <li>Give it text content</li>
+//                 <li>Give it a class name</li>
+//                 <li>Put it inside the div with id "root"</li>
+//             </ul>
+
+//         </main> 
+//     )
+// }
+
+/**
+ * Challenge (part 1):
+ * Create a custom "Page" component
+ * 
+ * It should return an ordered list with the reasons why you're
+ * excited to be learning React :)
+ * 
+ * Render the Page component.
+ */
+
+/**
+Challenge: 
+
+Part 2: 
+- Add a `<header>` element with an `<img />` element with the image of the 
+  React logo inside (src="react-logo.png") and make sure to set the 
+  width to something more manageable so it doesn't take up the whole screen.
+  Also, as always, you should include some alt text on the image.
+- Add an `<h1>` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list, then wrap
+  the `<h1>` and `<ol>` inside a `<main>` element to keep our semantic
+  structure flowing well.
+- Add a `<footer>` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
