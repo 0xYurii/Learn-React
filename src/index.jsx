55a5f00990d4cd9a45b153b3a573966d5bc9f809
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-08-16 11:29:48 +0100

    Refactor Header component to use CSS classes for styling and adjust image width

--- src/index.jsx
+++ src/index.jsx
@@ -2,26 +2,22 @@ import { createRoot } from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
 
 
-
 /**
  * Challenge:
  * 
- * Using flexbox, line up the `li`s horizontally and put them inline
- * with the React logo.
- * 
- * NOTE: for practice's sake, don't select any
- * elements, but use classes for all styling.
+ * Move the `width` style off the JSX and into CSS with a dedicated
+ * className on the img element, and change the width to 55px instead
  */
 
 function Header() {
     return (
-        <header>
-            <img src="src/react-1.svg" width="40" alt="React logo" />
+        <header className="header">
+            <img src="src/react-1.svg"  alt="React logo" className="img" />
             <nav>
                 <ul className="nav-list">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                    <li className="li">Pricing</li>
+                    <li className="li">About</li>
+                    <li className="li">Contact</li>
                 </ul>
             </nav>
         </header>
@@ -31,7 +27,7 @@ function Header() {
 function MainContent() {
     return (
         <main>
-            <h1>Reason I am excited to learn React</h1>
+            <h1 className="h1">Reason I am excited to learn React</h1>
             <ol>
                 <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
                 <li>I am more likely to get a job as a front end developer if I know React</li>
@@ -128,4 +124,15 @@ Part 2:
  * 
  * - Add a `nav` > `ul` > `li` (x3). The 3 items should say:
  *   "Pricing", "About", and "Contact"
+ */
+
+
+/**
+ * Challenge:
+ * 
+ * Using flexbox, line up the `li`s horizontally and put them inline
+ * with the React logo.
+ * 
+ * NOTE: for practice's sake, don't select any
+ * elements, but use classes for all styling.
  */
